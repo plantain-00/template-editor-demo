4830fcb3f732664c93bd011082b6d6aa8f124d4d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-07 07:55:41 +0000

    Deploy plantain-00/template-editor-demo to github.com/plantain-00/template-editor-demo.git:gh-pages

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import Component from 'vue-class-component'
 import { indexTemplateHtml, indexTemplateHtmlStatic } from './variables'
 import { styleGuide } from './data'
 import { CanvasState } from './canvas-state'
-import { CanvasMask } from './canvas-mask'
-import { EditorPanel } from './editor-panel'
-import { DraggingArea } from './dragging-area'
+import { MaskLayer } from './mask-layer'
+import { OperationPanel } from './operation-panel'
+import { DraggingLayer } from './dragging-layer'
+import { RenderLayer } from './render-layer'
 
-Vue.component('canvas-mask', CanvasMask)
-Vue.component('editor-panel', EditorPanel)
-Vue.component('dragging-area', DraggingArea)
+Vue.component('mask-layer', MaskLayer)
+Vue.component('dragging-layer', DraggingLayer)
+Vue.component('render-layer', RenderLayer)
+
+Vue.component('operation-panel', OperationPanel)
 
 @Component({
   render: indexTemplateHtml,
@@ -17,24 +20,6 @@ Vue.component('dragging-area', DraggingArea)
 })
 export class App extends Vue {
   canvasState = CanvasState.create(styleGuide)
-
-  get canvasStyle() {
-    return {
-      position: 'absolute',
-      width: this.canvasState.canvasWidth + 'px',
-      height: this.canvasState.canvasHeight + 'px',
-      overflow: 'hidden',
-      backgroundColor: '#ddd',
-    }
-  }
-
-  get styleGuideStyle() {
-    return {
-      transform: `scale(${this.canvasState.styleGuideScale}) translate(${this.canvasState.styleGuideTranslateX}px, ${this.canvasState.styleGuideTranslateY}px)`,
-      width: this.canvasState.styleGuideWidth + 'px',
-      height: this.canvasState.styleGuideHeight + 'px',
-    }
-  }
 }
 
 new App({ el: '#container' })
